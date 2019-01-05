ee6f63bc899ab05b86c0299e263e119cdfe45c1d
Chandler Davis <cdavis@MSC.local>
2019-01-05 12:09:59 -0600

    Initial Commit
    - There is a a green plane with a white capsule as the player
    - The camera moves behind the player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // The object we want the camera to follow.
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        //"transform" without an object refers to "this" object - the one the script is attached to. 
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))] // Adds a Rigidbody if object doesn't have one already.
+public class PlayerController : MonoBehaviour {
+
+    public float speed; // Movement speed
+    public float jumpHeight;
+
+    private bool isGrounded; // Is the player touching the ground?
+    private Rigidbody rb; // This object's rigid body
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() // Called before physics calculation
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        float jump = jumpHeight * Input.GetAxis("Jump");
+
+        Debug.Log(Input.GetAxis("Jump"));
+
+        rb.position += new Vector3(moveHorizontal, 0, moveVertical);
+
+        Vector3 movement = new Vector3(moveHorizontal, jump, moveVertical);
+
+        rb.AddForce(movement * speed);
+    } 
+
+}
